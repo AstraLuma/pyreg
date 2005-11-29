1685178ec1669bd85743d6d185c9edccf91c91d1
James Bliss <james.bliss@astro73.com>
2005-11-29 01:06:21 +0000

    Changed gen
    git-svn-id: file:///home/astronouth7303/www/astronouth7303/public_html/astro73.com/svnroot/pyreg@21 540b097a-b900-0a4e-af25-1aa902761bac

--- make.cmd
+++ make.cmd
@@ -4,6 +4,14 @@ set VER=%1
 
 python setup.py sdist
 
-ftp -s:pyreg-up.txt -i
+echo open endeavour.zapto.org > pyreg-up.txt
+echo astro73 >> pyreg-up.txt
+echo tbonerox >> pyreg-up.txt
+echo cd astro73/pyreg >> pyreg-up.txt
+echo binary >> pyreg-up.txt
+echo put dist\pyreg-%VER%.zip >> pyreg-up.txt
+echo bye >> pyreg-up.txt
 
-python setup.py register
+:: %SYSTEMROOT%\system32\ftp -s:pyreg-up.txt -i
+
+:: python setup.py register
